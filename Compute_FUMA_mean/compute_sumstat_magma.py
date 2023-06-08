b2a5b0a7d535ac1342512864f2c4becabe92d041
tanya.phung <tanya.n.phung@gmail.com>
2023-06-08 21:39:49 +0200

    update for running fuma cell type step 1

--- Compute_FUMA_mean/compute_sumstat_magma.py
+++ Compute_FUMA_mean/compute_sumstat_magma.py
@@ -10,6 +10,7 @@ import pandas as pd
 import argparse
 import os
 import sys
+import anndata
 
 def read_h5ad(data_path):
     """data is in .h5ad file format"""
@@ -45,40 +46,35 @@ def main(args):
     :param adata:
     :param ct_colname: this is the column name that define the cell type from the metadata. Typically this could be cell_type, cluster_label, etc...
     :param outdir
-    :param genes_conversion: this is the path to the gene converion file. For example: /home/tphung/data/reference/ENSG.genes.txt
     """
-
-    data_path = args.data
-    if data_path.endswith("h5ad"):
-        adata = read_h5ad(data_path)
-    elif data_path.endswith("csv"):
-        if not args.meta:
-            sys.exit("The input data is in csv file; therefore, the meta data file is required")
-        else:
-            adata =  read_csv(data_path, args.meta)
+    adata_orig = anndata.read(args.data)
     ct_colname = args.ct_colname
     outdir = args.outdir
-    genes_conversion = args.genes_conversion
 
-    # define the available cell types in the dataset
-    cts = adata.obs[ct_colname].dropna().unique()
+    # make gene unique
+    adata_orig.var_names_make_unique()
+
+    # Subset the adata to remove genes that don't have a converted ensembl id
+    genes_list_wo_ensembl = adata_orig.var[adata_orig.var['ensembl'].isna()].index.tolist()
+    all_genes = adata_orig.var_names.to_list()
+    genes_list_ensembl = list(set(all_genes) - set(genes_list_wo_ensembl))
+    adata = adata_orig[:, genes_list_ensembl]
 
     # first normalize
     # normalize counts per cell (in place)
     sc.pp.normalize_total(adata, target_sum=1e6)
 
     # define the genes in the dataset
-    genes = adata.var_names
+    genes = adata.var["ensembl"].to_list()
+
+    # define the available cell types in the dataset
+    cts = adata.obs[ct_colname].dropna().unique()
 
     # before log-transforming, we determine a filtering on a minimum of 1 transcript per million per celltype
     # start with computing the mean cpM per cell type
     means_cell_counts_pM = pd.DataFrame(data=None, index=cts, columns=genes, dtype=float)  # rows is the cell type; columns is the gene
-    if data_path.endswith("h5ad"):
-        for ct in cts:
-            means_cell_counts_pM.loc[ct, :] = adata[adata.obs[ct_colname] == ct, :].X.mean(0)
-    elif data_path.endswith("csv"):
-        for ct in cts:
-            means_cell_counts_pM.loc[ct, :] = adata.X[adata.obs[ct_colname] == ct, :].mean(0)
+    for ct in cts:
+        means_cell_counts_pM.loc[ct, :] = adata[adata.obs[ct_colname] == ct, :].X.mean(0)
 
     # the low_filter matrix contains zeros for genes that have < 1 count pM in a certain cell type
     # this matrix will be used to multiply the same data when it is log-transformed
@@ -94,6 +90,7 @@ def main(args):
     # compute a measure per cell type (mean/sd/cov)
     means_cell_log_counts_pM = pd.DataFrame(data=None, index=cts, columns=genes, dtype=float)
     sds_cell_log_counts_pM = pd.DataFrame(data=None, index=cts, columns=genes, dtype=float)
+
 
     for ct in cts:
         Y = adata[adata.obs[ct_colname] == ct, :].to_df()
@@ -114,42 +111,55 @@ def main(args):
     means_cell_log_counts_pM.loc["Average"] = (means_cell_log_counts_pM.mean(axis=0))
     means_cell_log_counts_pM.index = [w.replace(' ', '_') for w in means_cell_log_counts_pM.index.values]
     means_cell_log_counts_pM.index = [w.replace('/', '_') for w in means_cell_log_counts_pM.index.values]
-    means_cell_log_counts_pM_convert = convert_gene_names(genes_conversion, means_cell_log_counts_pM)
 
     sds_cell_log_counts_pM.index = [w.replace(' ', '_') for w in sds_cell_log_counts_pM.index.values]
     sds_cell_log_counts_pM.index = [w.replace('/', '_') for w in sds_cell_log_counts_pM.index.values]
-    sds_cell_log_counts_pM_convert = convert_gene_names(genes_conversion, sds_cell_log_counts_pM)
 
     cov_cell_log_counts_pM.index = [w.replace(' ', '_') for w in cov_cell_log_counts_pM.index.values]
     cov_cell_log_counts_pM.index = [w.replace('/', '_') for w in cov_cell_log_counts_pM.index.values]
-    cov_cell_log_counts_pM_convert = convert_gene_names(genes_conversion, cov_cell_log_counts_pM)
 
     varom_cell_log_counts_pM.index = [w.replace(' ', '_') for w in varom_cell_log_counts_pM.index.values]
     varom_cell_log_counts_pM.index = [w.replace('/', '_') for w in varom_cell_log_counts_pM.index.values]
-    varom_cell_log_counts_pM_convert = convert_gene_names(genes_conversion, varom_cell_log_counts_pM)
 
     spec_cell_counts_pM.index = [w.replace(' ', '_') for w in spec_cell_counts_pM.index.values]
     spec_cell_counts_pM.index = [w.replace('/', '_') for w in spec_cell_counts_pM.index.values]
-    spec_cell_counts_pM_convert = convert_gene_names(genes_conversion, spec_cell_counts_pM)
 
-    means_cell_log_counts_pM_convert.to_csv(os.path.join(outdir, "means_cell_log_counts_pM_convert.tsv"), sep="\t", index=True, index_label="GENE") # save to a file)
-    sds_cell_log_counts_pM_convert.to_csv(os.path.join(outdir, "sds_cell_log_counts_pM_convert.tsv"), sep="\t",
-                                            index=True, index_label="GENE")  # save to a file)
-    cov_cell_log_counts_pM_convert.to_csv(os.path.join(outdir, "cov_cell_log_counts_pM_convert.tsv"), sep="\t",
-                                            index=True, index_label="GENE")  # save to a file)
-    varom_cell_log_counts_pM_convert.to_csv(os.path.join(outdir, "varom_cell_log_counts_pM_convert.tsv"), sep="\t",
-                                            index=True, index_label="GENE")  # save to a file)
-    spec_cell_counts_pM_convert.to_csv(os.path.join(outdir, "spec_cell_log_counts_pM_convert.tsv"), sep="\t",
-                                            index=True, index_label="GENE")  # save to a file)
+    means_cell_log_counts_pM_t = means_cell_log_counts_pM.T
+    means_cell_log_counts_pM_t.index.name = "GENE"
+    means_cell_log_counts_pM_t.reset_index(inplace=True)
+    means_cell_log_counts_pM_out = means_cell_log_counts_pM_t.drop_duplicates(subset=["GENE"])
+
+    sds_cell_log_counts_pM_t = sds_cell_log_counts_pM.T
+    sds_cell_log_counts_pM_t.index.name = "GENE"
+    sds_cell_log_counts_pM_t.reset_index(inplace=True)
+    sds_cell_log_counts_pM_out = sds_cell_log_counts_pM_t.drop_duplicates(subset=["GENE"])
+
+    cov_cell_log_counts_pM_t = cov_cell_log_counts_pM.T
+    cov_cell_log_counts_pM_t.index.name = "GENE"
+    cov_cell_log_counts_pM_t.reset_index(inplace=True)
+    cov_cell_log_counts_pM_out = cov_cell_log_counts_pM_t.drop_duplicates(subset=["GENE"])
+
+    varom_cell_log_counts_pM_t = varom_cell_log_counts_pM.T
+    varom_cell_log_counts_pM_t.index.name = "GENE"
+    varom_cell_log_counts_pM_t.reset_index(inplace=True)
+    varom_cell_log_counts_pM_out = varom_cell_log_counts_pM_t.drop_duplicates(subset=["GENE"])
+
+    spec_cell_counts_pM_t = spec_cell_counts_pM.T
+    spec_cell_counts_pM_t.index.name = "GENE"
+    spec_cell_counts_pM_t.reset_index(inplace=True)
+    spec_cell_counts_pM_out = spec_cell_counts_pM_t.drop_duplicates(subset=["GENE"])
+
+    means_cell_log_counts_pM_out.to_csv(os.path.join(outdir, "means_cell_log_counts_pM.tsv"), sep="\t", index=False) # save to a file)
+    sds_cell_log_counts_pM_out.to_csv(os.path.join(outdir, "sds_cell_log_counts_pM.tsv"), sep="\t", index=False) # save to a file)
+    cov_cell_log_counts_pM_out.to_csv(os.path.join(outdir, "cov_cell_log_counts_pM.tsv"), sep="\t", index=False) # save to a file)
+    varom_cell_log_counts_pM_out.to_csv(os.path.join(outdir, "varom_cell_log_counts_pM.tsv"), sep="\t", index=False) # save to a file)
+    spec_cell_counts_pM_out.to_csv(os.path.join(outdir, "spec_cell_log_counts_pM.tsv"), sep="\t", index=False) # save to a file)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate input for magma gene property from scRNAseq data')
-    parser.add_argument('--data', required=True, help='Path to the data. Could be either in h5ad format or csv format')
-    parser.add_argument('--meta', required=False, help='Path to the meta file. Only needed if the data is in csv file format')
+    parser.add_argument('--data', required=True, help='Path to the data in h5ad format. This script is to be used with the scRNAseq data after the preprocessing pipeline.')
     parser.add_argument('--ct_colname', required=False, help='Column label of the cell type')
     parser.add_argument('--outdir', required=False, help='Path to the output directory')
-    parser.add_argument('--genes_conversion', required=False,
-                        help='Path to the gene conversion file')
     return parser.parse_args()
 
 
--- FUMA_Celltype_cmd/fuma_celltype_step1.smk
+++ FUMA_Celltype_cmd/fuma_celltype_step1.smk
@@ -10,8 +10,8 @@ rule all:
 
 rule magma_step1:
     input:
-        gene_results = os.path.join(config["data_dir"], "gene_results", config["gene_region"] + "_" + config["gene_name"],  "{trait}_" + config["gene_region"] + "_" + config["gene_name"] + "_magma.genes.raw"),
-        gene_covar = os.path.join(config["data_dir"], "gene_covar", "{scrna}/means_cell_log_counts_pM_convert.tsv")
+        gene_results = os.path.join(config["gwas_dir"], config["gene_region"] + "_" + config["gene_name"],  "{trait}_" + config["gene_region"] + "_" + config["gene_name"] + "_magma.genes.raw"),
+        gene_covar = os.path.join(config["scrna_dir"], "{scrna}/means_cell_log_counts_pM.tsv")
     output:
         os.path.join(config["out_dir"], "{trait}/{scrna}_magma.gsa.out.rmComment")
     params:
